ceb78c9bb7215214a533f385d415c6e7a1da983d
emii <eizquierd@gmail.com>
2013-05-20 11:10:28 +0200

    modified for ubuntu

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-cd /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/
-rm -rf output
-python /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/slidesMD/m2d /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/slidesMD/presentationMD.md /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/slidesMD/deck.html > /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/contents/slides/presentation.md
+source /home/$USER/sndbxbe/bin/activate
+cd /home/$USER/sndbxbe/sndbxbe/slidesMD
+#rm -rf /home/$USER/sndbxbe/sndbxbe/output
+python /home/$USER/sndbxbe/sndbxbe/slidesMD/m2d /home/$USER/sndbxbe/sndbxbe/slidesMD/presentationMD.md /home/$USER/sndbxbe/sndbxbe/slidesMD/deck.html > /home/$USER/sndbxbe/sndbxbe/contents/slides/presentation.md
+cd /home/$USER/sndbxbe/sndbxbe/
 pelican -s "conf.py"
-cd /Users/e.rangel/Documents/workpath/sndbxbe/www-sndbxbe/output/
+cd /home/$USER/sndbxbe/sndbxbe/output/ 
 mv theme/ static/
 python -m SimpleHTTPServer
-
